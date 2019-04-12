69352d686316b50be705ec496646098cb4d55887
qijun_jiang@163.com <jiangqijun@globalegrow.com>
2019-04-12 18:16:03 +0800

    xinzeng

--- usermanage/urls.py
+++ usermanage/urls.py
@@ -12,10 +12,11 @@ from usermanage import views
 # router.register(r'snippets', views.SnippetViewSet)
 # router.register(r'users', views.UserViewSet )
 urlpatterns = [
-    path(r'login/', views.login),
-    path(r'index/', views.index),
-    path(r'nocookie/', views.nocookie),
-    path(r'operate_session/', views.operate_session),
-    path(r'login_view/', views.login_view),
-    path(r'handle_classes/', views.handle_classes),
+    # path(r'login/', views.login),
+    # path(r'index/', views.index),
+    # path(r'nocookie/', views.nocookie),
+    # path(r'operate_session/', views.operate_session),
+    # path(r'login_view/', views.login_view),
+    # path(r'handle_classes/', views.handle_classes),
+    path(r'register/', views.user_register),
 ]
--- usermanage/views.py
+++ usermanage/views.py
@@ -36,84 +36,88 @@ from django.contrib.sessions.models import Session
 #     return render(req, 'index.html', {'cookie': cookie})
 
 
-def nocookie(req):
-    cookie = req.COOKIES.get('user_id')
-    return render(req, 'nocookie.html', {'cookie': cookie})
+# def nocookie(req):
+#     cookie = req.COOKIES.get('user_id')
+#     return render(req, 'nocookie.html', {'cookie': cookie})
+#
+#
+# def login(req):
+#     message = '默认值'
+#     coo = req.COOKIES.get('sessionid')
+#     print(coo)
+#     if req.method == 'POST':
+#         u = req.POST.get('username')
+#         p = req.POST.get('password')
+#         obj = models.Administrator.objects.filter(username=u, password=p)
+#         c = obj.count()
+#         if c:
+#             print('################')
+#             req.session['is_login'] = True #只有在这里设置了seesion才会在数据库存在
+#             req.session['user_id'] = obj[0].id
+#             print(type(obj)) #<class 'django.db.models.query.QuerySet'>
+#             print(type(obj[0])) #<class 'usermanage.models.Administrator'>
+#             print(type(obj.values('id'))) #<class 'django.db.models.query.QuerySet'>
+#             # sess = req.session
+#             # s = Session.objects.first()
+#             # print(sess)  # <django.contrib.sessions.backends.db.SessionStore object at 0x052A2730> session对象
+#             # print(s.session_data)  # NzFmNWRmMmNhZDI2YmE3NzhhMGNmZjU4NzAzZTMyYjFlMmZkNTk3Mjp7ImlzX2xvZ2luIjp0cnVlLCJ1c2VyX2lkIjoxfQ==  session值
+#             # print(s.get_decoded())  # {'is_login': True, 'user_id': 1} seesion解密后的值
+#             return redirect('/index/')
+#         else:
+#             message='设置seesion失败'
+#     return render(req, 'login.html', {'cookie':  message})
+#
+#
+# def index(req):
+#     s = req.session['is_login']
+#     print(req.session['is_login'])
+#     return render(req, 'index.html', {'cookie': "seeion is %s"%s})
+#
+#
+# def operate_session(req):
+#     s1=Session.objects.get(pk='infp6edy94scjxsmtvbz0so5w835n2kr') #获取session的值seesion_data
+#     print(s1.get_decoded())#解码seesion_data
+#     s2 = Session.objects.get(pk='l8j4n5e7jmmxm1go75ybkn1r2aidxmax')
+#     print(s2.get_decoded())
+#     sess = req.session.get('is_login',None) #获取seesion值
+#     print(req.COOKIES.get('sessionid')) #这里放到cookie里面的值就是session的id
+#     #del req.session['user_id'] #删除seesion
+#     # 所有 键、值、键值对
+#     req.session.keys()#所有的键
+#     req.session.values()#所有的值
+#     print(req.session.items())#所有键值对
+#     req.session.clear_expired() #清除过期的
+#     req.session.delete("session_key") #清除所有的键值对
+#     print(req.session.items())  # 所有键值对
+#     return render(req, 'operate_session.html', {'session': 'session is %s'%sess})
+#
+#
+# from django.contrib import auth
+# def login_view(req):
+#     if req.method == 'POST':
+#         username=req.POST.get('username')
+#         password=req.POST.get('password')
+#         user = auth.authenticate(username=username, password=password)
+#         if user is not None and user.is_active:
+#             auth.login(req, user)
+#             return HttpResponseRedirect("/account/loggedin/")
+#     return render(req, 'login_view.html')
+#
+#
+# def auth(fun):
+#     def inner(request, *args, **kwargs):
+#         is_login = request.session.get('is_login')
+#         if is_login:
+#             return fun(request, *args, **kwargs)
+#         else:
+#             return redirect('/login/')
+#     return inner
+#
+# @auth
+# def handle_classes(request):
+#     user_id = request.session.get('user_id')
+#     return render(request, 'class.html', {'userid': user_id})
 
 
-def login(req):
-    message = '默认值'
-    coo = req.COOKIES.get('sessionid')
-    print(coo)
-    if req.method == 'POST':
-        u = req.POST.get('username')
-        p = req.POST.get('password')
-        obj = models.Administrator.objects.filter(username=u, password=p)
-        c = obj.count()
-        if c:
-            print('################')
-            req.session['is_login'] = True #只有在这里设置了seesion才会在数据库存在
-            req.session['user_id'] = obj[0].id
-            print(type(obj)) #<class 'django.db.models.query.QuerySet'>
-            print(type(obj[0])) #<class 'usermanage.models.Administrator'>
-            print(type(obj.values('id'))) #<class 'django.db.models.query.QuerySet'>
-            # sess = req.session
-            # s = Session.objects.first()
-            # print(sess)  # <django.contrib.sessions.backends.db.SessionStore object at 0x052A2730> session对象
-            # print(s.session_data)  # NzFmNWRmMmNhZDI2YmE3NzhhMGNmZjU4NzAzZTMyYjFlMmZkNTk3Mjp7ImlzX2xvZ2luIjp0cnVlLCJ1c2VyX2lkIjoxfQ==  session值
-            # print(s.get_decoded())  # {'is_login': True, 'user_id': 1} seesion解密后的值
-            return redirect('/index/')
-        else:
-            message='设置seesion失败'
-    return render(req, 'login.html', {'cookie':  message})
-
-
-def index(req):
-    s = req.session['is_login']
-    print(req.session['is_login'])
-    return render(req, 'index.html', {'cookie': "seeion is %s"%s})
-
-
-def operate_session(req):
-    s1=Session.objects.get(pk='infp6edy94scjxsmtvbz0so5w835n2kr') #获取session的值seesion_data
-    print(s1.get_decoded())#解码seesion_data
-    s2 = Session.objects.get(pk='l8j4n5e7jmmxm1go75ybkn1r2aidxmax')
-    print(s2.get_decoded())
-    sess = req.session.get('is_login',None) #获取seesion值
-    print(req.COOKIES.get('sessionid')) #这里放到cookie里面的值就是session的id
-    #del req.session['user_id'] #删除seesion
-    # 所有 键、值、键值对
-    req.session.keys()#所有的键
-    req.session.values()#所有的值
-    print(req.session.items())#所有键值对
-    req.session.clear_expired() #清除过期的
-    req.session.delete("session_key") #清除所有的键值对
-    print(req.session.items())  # 所有键值对
-    return render(req, 'operate_session.html', {'session': 'session is %s'%sess})
-
-
-from django.contrib import auth
-def login_view(req):
-    if req.method == 'POST':
-        username=req.POST.get('username')
-        password=req.POST.get('password')
-        user = auth.authenticate(username=username, password=password)
-        if user is not None and user.is_active:
-            auth.login(req, user)
-            return HttpResponseRedirect("/account/loggedin/")
-    return render(req, 'login_view.html')
-
-
-def auth(fun):
-    def inner(request, *args, **kwargs):
-        is_login = request.session.get('is_login')
-        if is_login:
-            return fun(request, *args, **kwargs)
-        else:
-            return redirect('/login/')
-    return inner
-
-@auth
-def handle_classes(request):
-    user_id = request.session.get('user_id')
-    return render(request, 'class.html', {'userid': user_id})
+def user_register(request):
+    pass
